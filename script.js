96c265f8788727faaccf9a41439f77802d779758
JenniNick-1202 <156542526+JenniNick-1202@users.noreply.github.com>
2024-03-17 22:11:30 +0000

    added JS shop functionality

--- script.js
+++ script.js
@@ -1,30 +1,81 @@
-let hotDrinksMenu = [
-        {name: "americano", size: ["small", "medium", "large"], price: [2.50, 3.00, 3.50]},
-        {name: "flatWhite", size: ["small", "medium", "large"], price: [2.75, 3.25, 3.75]},
-        {name: "latte", size: ["small", "medium", "large"], price: [3.00, 3.50, 4.00]},
-        {name: "cappuccino", size: ["small", "medium", "large"], price: [3.00, 3.50, 4.00]},
-        {name: "mocha", size: ["small", "medium", "large"], price: [3.00, 3.50, 4.00]},
-        {name: "macchiato", size: ["small", "medium", "large"], price: [3.50, 4.00, 4.50]},
-        {name: "tea", size: ["small", "medium", "large"], price: [1.00, 1.50, 2.00]}
+let shop = document.getElementById("shop");
+
+let shopItemsData = [ {
+    id: "americano",
+    name: "Americano", 
+    size: ["small", "medium", "large"], 
+    price: [2.50, 3.00, 3.50],
+    desc: "Our freshly brewed signature black coffee.",
+    images: "images/Americano.jpg"
+}, {
+    id: "flatWhite",
+    name: "FlatWhite", 
+    size: ["small", "medium", "large"], 
+    price: [2.50, 3.00, 3.50],
+    desc: "Freshly brewed black coffee with steamed milk.",
+    images: "images/flatWhite.jpg"
+}, {
+     id: "latte",
+    name: "Latte", 
+    size: ["small", "medium", "large"], 
+    price: [3.00, 3.50, 4.00],
+    desc: "Our signature coffee, a creamy and frothy delight.",
+    images: "images/latte.jpg"
+}, {
+    id: "cappuccino",
+    name: "Cappuccino", 
+    size: ["small", "medium", "large"], 
+    price: [3.00, 3.50, 4.00],
+    desc: "Our signature coffee with steamed and foamed milk.",
+    images: "images/Cappuccino.jpg"
+},
 ];
 
-function selectItem() {
-    const selectedSize = document.getElementById("size").value;
-    switch (selectedSize) {
-        case "small":
-            document.getElementById("price-display").textContent = "Price: £2.50";
+let generateShop = () => {
+    return (shop.innerHTML = shopItemsData
+        .map((x) => {
+            let {id, name, size, price, desc, images} = x;
+        return `
+        <div class="menu-card">
+        <img src= ${images} alt=${name} id="menu-image"/>
+            <div class="menu-card-desc">
+                    <h3 class="flavor">${name}</h3>
+                    <p>${desc}</p>
+                    <select id="size" onchange="displayPrice(this.value, '${id}')">
+                        <option value="pleaseSelect">Please Select</option>
+                        <option value="small">Small</option>
+                        <option value="medium">Medium</option>
+                        <option value="large">Large</option>
+                 </select>
+            <p class="price" id="price-display-${id}"></p>
+            <button type="button" id="add-to-basket-button" onclick="addToBasket('${id}')">Add to Basket</button>
+        </div>
+    </div>
+        `;
+    }).join(""));
+};
+
+let displayPrice = (size, id) => {
+    let item = shopItemsData.find(x => x.id === id);
+    let price;
+    switch(size) {
+        case 'small':
+            price = item.price[0];
             break;
-        case "medium":
-            document.getElementById("price-display").textContent = "Price: £3.00";
+        case 'medium':
+            price = item.price[1];
             break;
-        case "large":
-            document.getElementById("price-display").textContent = "Price: £3.50";
+        case 'large':
+            price = item.price[2];
             break;
         default:
-            document.getElementById("price-display").textContent = "Please select a size.";
-            break;
+            price = 'Please select a size';
     }
+    price = '£' + price.toFixed(2);
+    document.getElementById('price-display-' + id).innerText = price;
 }
 
-let shop = document.getElementById("shop");
-console.log(shop);
+generateShop();
+
+
+
